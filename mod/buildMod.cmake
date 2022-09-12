41a42e2c5ab4e8656ed77daa93bbb04a937183d9
Molybdocene-dichloride <minex123448999@gmail.com>
2022-09-12 10:58:18 +0300

    build config generation method

--- mod/buildMod.cmake
+++ mod/buildMod.cmake
@@ -21,7 +21,7 @@ function(copyResources PRJ_DIR PATHS OUTPUT_DIR REWRITE) #without changes. only 
             #message(${file})
             file(RELATIVE_PATH relfile ${PRJ_DIR} ${file})
             
-            if(${REWRITE} OR (NOT EXISTS ${PRJ_DIR}${OUTPUT_DIR}${relfile}))
+            if(${REWRITE} OR (NOT EXISTS ${PRJ_DIR}${OUTPUT_DIR}/${relfile}))
                 message(${PRJ_DIR}${OUTPUT_DIR}/${relfile})
                 configure_file(
                     ${file}
@@ -33,10 +33,21 @@ function(copyResources PRJ_DIR PATHS OUTPUT_DIR REWRITE) #without changes. only 
     endforeach()
 endfunction()
 
-function(generateBuildConfig OUTPUT_DIR)
-    configure_file(
-        ${file}
-        ${PRJ_DIR}${OUTPUT_DIR}/${relfile}
-        COPYONLY
-    )
+function(generateBuildConfig PRJ_DIR OUTPUT_DIR RESOURCE_PATHS LIBRARY_PATHp REWRITE)
+    list(GET RESOURCE_PATHS 0 RESOURCE_PATHp0)
+    list(GET RESOURCE_PATHS 1 RESOURCE_PATHp1)
+    
+    set(RESOURCE_PATH0 \"${RESOURCE_PATHp0}\")
+    set(RESOURCE_PATH1 \"${RESOURCE_PATHp1}\")
+    set(LIBRARY_PATH \"${LIBRARY_PATHp}\")
+    
+    if(${REWRITE} OR (NOT EXISTS ${PRJ_DIR}${OUTPUT_DIR}/build.config))
+        configure_file(
+            ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/build.config.in
+            ${PRJ_DIR}${OUTPUT_DIR}/build.config
+            @ONLY
+        )
+    endif()
 endfunction()
+
+function(add_ts_mod)
